2d970bcbbc37a420a1c34d3da2a8fb919f67dd81
DESKTOP-NCG0MQ4\adelq <adel.elmaghloub7@gmail.com>
2025-12-30 22:15:28 +0300

    Updated pipeline

--- src/api/main.py
+++ src/api/main.py
@@ -33,9 +33,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # 3. Add a basic route to trigger metrics
-@app.get("/")
-async def root():
-    return {"message": "House Price Prediction API is online"}
+
 
 Instrumentator().instrument(app).expose(app)
 
