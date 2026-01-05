a0db824a5f775510c9212aae6f9b19bb34227e63
DESKTOP-NCG0MQ4\adelq <adel.elmaghloub7@gmail.com>
2026-01-05 14:11:44 +0300

    Added Metrics section

--- src/api/main.py
+++ src/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from inference import predict_price, batch_predict
 from schemas import HousePredictionRequest, PredictionResponse
-#from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator import Instrumentator
 #from prometheus_client import start_http_server
 #import threading
 
@@ -37,7 +37,9 @@ app.add_middleware(
 # 3. Add a basic route to trigger metrics
 
 
-#Instrumentator().instrument(app).expose(app) 
+Instrumentator().instrument(app).expose(app) 
+
+  
 
 
 # Health check endpoint
